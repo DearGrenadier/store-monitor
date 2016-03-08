591f81ef6ca21522c4c66af7cd58493a94090320
Dima Nazarchuk <gitnazarchuk@gmail.com>
2016-03-09 01:14:24 +0300

    Fixes

--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -3,4 +3,14 @@ class LineItem < ApplicationRecord
   belongs_to :order
 
   validates :quantity, numericality: { only_integer: true, greater_than: 0 }
+
+  after_destroy :free_products
+
+  private
+
+  def free_products
+    current_amount = product_attr.amount
+    new_amount = current_amount + quantity
+    product_attr.update!(amount: new_amount)
+  end
 end
