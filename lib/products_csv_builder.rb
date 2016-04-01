fb70af5c324106ba4fac109d36a782155cf7a240
Dima Nazarchuk <dmitry@nazarchuk.cf>
2016-04-01 03:07:56 +0300

    Add price format

--- lib/products_csv_builder.rb
+++ lib/products_csv_builder.rb
@@ -1,6 +1,9 @@
 require 'csv'
+require 'action_view'
 
 class ProductsCSVBuilder
+  extend ActionView::Helpers::NumberHelper
+
   HEADER = %w( Наименование Размер Цена(руб.) Количество )
 
   def self.build 
@@ -9,9 +12,13 @@ class ProductsCSVBuilder
       Product.all.each do |product|
         product.product_attrs.map.with_index do |product_attr, index|
           first_column = index.zero? ? product.name : nil
-          csv << [first_column, product_attr.size.value, product_attr.price, product_attr.amount]
+          csv << [first_column, product_attr.size.value, format_price(product_attr.price), product_attr.amount]
         end
       end
     end
+  end
+
+  def self.format_price(price)
+    number_to_currency(price, delimiter: ' ', precision: 0, format: '%n')
   end
 end
